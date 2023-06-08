59408305dc0fef89890e551a80931b5d34d270c7
SANILA-SASIKARAN <134697256+SANILA-SASIKARAN@users.noreply.github.com>
2023-06-08 20:16:29 +0530

    Add files via upload

---
+++ height.weight.py
@@ -0,0 +1,15 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/``````````/AppData/Local/Programs/Python/Python311/12 age..py
+>>> age=12
+>>> print(age)
+12
+>>> 
+== RESTART: C:/Users/``````````/AppData/Local/Programs/Python/Python311/30.py ==
+>>> weight=30
+>>> print(weight)
+30
+>>> height=134
+>>> print(height)
+134
